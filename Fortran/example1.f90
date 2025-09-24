731077e18b9c1e724f8d35f63eb08b390166a2c6
alexeykiselyk <alex.kiselik@gmail.com>
2025-09-24 18:59:12 +0100

    more additions to practice code

--- Fortran/example1.f90
+++ Fortran/example1.f90
@@ -2,8 +2,15 @@ PROGRAM example1
 ! This is a comment. Comments in Fortran start with an exclamation mark (!)
     IMPLICIT NONE
     INTEGER :: hours, mins, secs, temp
+    REAL :: x, y
     PRINT *, 'Type the hours, minutes and seconds'
     READ *, hours, mins, secs
     temp = 60*(hours*60 + mins) + secs
     PRINT *, 'Time in seconds =',temp
+
+    print *, 'Now enter two numbers'
+    read(*,*) x, y
+
+    print *, 'The sum and product of the numbers are: ', x+y, x*y
+    
 END PROGRAM example1
